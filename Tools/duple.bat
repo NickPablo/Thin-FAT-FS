52fe493c3575b8f4787e1fb052feaedce526ecfa
Nick Pablo <nickcpablo@yahoo.com>
2021-12-25 21:35:51 +0800

    Delete Tools directory

--- Tools/duple.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo This batch file is part of the TFS library
-pause
-set /p Input= Enter new file name:
-copy /b 1kblank.txt %Input%
-REM the next line will create a 64k file
-FOR /L %%y IN (0, 1, 1023) DO COPY /b %Input%+1kblank.txt %Input%
-echo Done!
